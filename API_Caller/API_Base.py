b114641e3b144eb78f89df5c145fa0ab3b43cd96
SpiderCoop <david.jimenez.cooper@gmail.com>
2025-02-21 14:38:22 -0600

    Create base api father

---
+++ API_Caller/API_Base.py
@@ -0,0 +1,25 @@
+import requests
+
+class API_Base:
+    def __init__(self,api_key, base_url):
+        self.api_key = api_key
+        self.base_url = base_url
+
+    def _make_request(self, endpoint, headers=None, params=None):
+        # Construir la URL
+        url = self.base_url + endpoint
+        print(url)
+
+        if headers is None:
+            headers = {}
+        if params is None:
+            params = {}
+        
+        # Realizar la solicitud GET
+        response = requests.get(url, headers=headers, params=params)
+
+        # Verificar si la solicitud fue exitosa
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception (f"Error: {response.status_code} - {response.text}")
