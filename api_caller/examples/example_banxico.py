7e87c09d841e73e02482185c42419c68c1dee9c0
SpiderCoop <david.jimenez.cooper@gmail.com>
2025-02-24 17:43:34 -0600

    Corrections on imports

--- api_caller/examples/example_banxico.py
+++ api_caller/examples/example_banxico.py
@@ -1,24 +1,21 @@
 # Ejemplo de uso
 import sys
 import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from banxico.sie import BanxicoSIE
 import pandas as pd
-import os
+from dotenv import load_dotenv
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+from api_caller.banxico import Banxico_SIE
 
 # Carga variables de un archivo .env (para almacenar el token de la API de Banxico)
-from dotenv import load_dotenv
 load_dotenv()
 Banxico_SIE_Token = os.environ.get("Banxico_SIE_Token")
 
 # Ejemplo de uso de la clase API_Banxico
-banxico_api = BanxicoSIE(Banxico_SIE_Token)
+banxico_api = Banxico_SIE(Banxico_SIE_Token)
 serie_id = ['SR17622', 'SF61745', 'SP68257', 'SF43718']
 
 # Obtener datos de la serie desde 2023-01-01 hasta hoy
 serie = banxico_api.get_data(serie_id, fecha_inicio='2023-01-01',variacion='PorcAnual')
-
-print('\n')
 print(serie.loc[pd.date_range(start='2024-01-01', end='2024-12-01', freq='QS-MAR').date,'SR17622'])
 print('\n')
--- api_caller/examples/example_inegi.py
+++ api_caller/examples/example_inegi.py
@@ -1,15 +1,13 @@
 # Ejemplo de uso
 import sys
 import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from inegi.bie import INEGI_BIE
 import pandas as pd
-import os
+from dotenv import load_dotenv
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+from api_caller.inegi import INEGI_BIE
 
 # Carga variables de un archivo .env (para almacenar el token de la API de INEGI)
-from dotenv import load_dotenv
 load_dotenv()
 INEGI_BIE_Token = os.environ.get("INEGI_BIE_Token")
 
@@ -19,7 +17,6 @@ serie_id=['736183','628208']
 
 # Obtener datos de las series de INEGI 628208, 736183 (PIB constante 2018 desestacionalizado var anual)
 serie = inegi_api.get_data(serie_id,last_data=False)
-
 print(serie.loc[pd.date_range(start='2024-01-01', end='2024-12-01', freq='QS-MAR').date,'736183'])
 print('\n')
 
