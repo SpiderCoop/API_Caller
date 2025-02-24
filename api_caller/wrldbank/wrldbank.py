9435bd8f6c0388dc13a9268cdb6874ba1f80e9ec
SpiderCoop <david.jimenez.cooper@gmail.com>
2025-02-24 17:20:49 -0600

    Creation of new module for the use of the world bank API

---
+++ api_caller/wrldbank/wrldbank.py
@@ -0,0 +1,36 @@
+
+# Librerias necesarias -------------------------------------------------------------------------
+
+import requests
+import pandas as pd
+
+from ..baseapi.baseapi import BaseAPI
+
+# Clase ---------------------------------------------------------------------------------------
+
+class WorldBank:
+    def __init__(self, api_key):
+        super().__init__(api_key, "https://api.worldbank.org/v2")
+
+    def get_data(self, indicator_id, country_code, start_date, end_date):
+        url = f'{self.base_url}/country/{country_code}/indicator/{indicator_id}'
+        params = {
+            'format': 'json',
+            'date': f'{start_date}:{end_date}',
+            'per_page': 100,
+            'api_key': self.api_key
+        }
+
+        response = requests.get(url, params=params)
+        data = response.json()
+
+        # Extract data and metadata
+        data_list = data[1]
+        metadata = data[0]
+
+        # Convert data to dataframe
+        df = pd.DataFrame(data_list)
+        df['date'] = pd.to_datetime(df['date'])
+        df['value'] = pd.to_numeric(df['value'])
+
+        return df, metadata
