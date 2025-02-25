aceacca8a886f6b740d7bbdec2eb089301963685
SpiderCoop <david.jimenez.cooper@gmail.com>
2025-02-24 18:34:40 -0600

    Update

--- api_caller/fed/fed.py
+++ api_caller/fed/fed.py
@@ -20,7 +20,7 @@ class Fred(BaseAPI):
         
         # Validar los tipos de datos de los parámetros
         if not isinstance(last_data, bool):
-            raise ValueError(f"ult_obs debe ser un valor booleano.")
+            raise ValueError(f"last_data must be a booleano.")
         
         # Validar los tipos de datos de las series y los parámetros
         if isinstance(serie_id, str):
@@ -30,6 +30,9 @@ class Fred(BaseAPI):
         else:
             raise ValueError("El 'serie_id' debe ser una cadena de texto o una lista de cadenas de texto.")
         
+        if len(serie_id) > 1 and last_data:
+            raise ValueError("last_data must be False if multiple series are provided.")
+        
 
         # Definir la URL de la API con el ID de la serie para obtener los datos de las series
         if last_data:
@@ -38,7 +41,7 @@ class Fred(BaseAPI):
                 raise ValueError("If last_data is True, start_time and end_time cannot be provided.")
             
             # Definir la URL de la API con el ID de la serie para obtener los datos de la última observación
-            endpoint = f"/series?{','.join(serie_id)}"
+            endpoint = f"/series?series_id={','.join(serie_id)}"
         
         else:
             # Definir los parámetros de las fechas si se proporcionan
@@ -51,7 +54,7 @@ class Fred(BaseAPI):
                     raise ValueError("The provided dates must be in format 'YYYY-MM-DD'.")
 
             try:
-                fecha_fin = str(datetime.strptime(fecha_fin, '%Y-%m-%d').date())
+                end_time = str(datetime.strptime(end_time, '%Y-%m-%d').date())
             except ValueError:
                 raise ValueError("The provided dates must be in format 'YYYY-MM-DD'.")
             
@@ -63,14 +66,14 @@ class Fred(BaseAPI):
 
             # Agregar los parámetros de las fechas a la URL si se proporcionan
             if start_time != None:
-                date_params.append(f'realtime_start={start_time}')
+                date_params.append(f'observation_start={start_time}')
             
-            date_params.append(f'realtime_end={end_time}')
+            date_params.append(f'observation_end={end_time}')
 
             if date_params:
-                endpoint = f"/series/observations?{','.join(serie_id)}&{'&'.join(date_params)}"
+                endpoint = f"/series/observations?series_id={','.join(serie_id)}&{'&'.join(date_params)}"
                 
-        endpoint += f"&api_key={self._BaseAPI__api_key}&format=json"
+        endpoint += f"&api_key={self._BaseAPI__api_key}&file_type=json"
 
         return endpoint
     
@@ -88,7 +91,7 @@ class Fred(BaseAPI):
                                     Por defecto es False.
             fecha_inicio (datetime, optional): La fecha de inicio de consulta tipo datetime para obtener datos en formato 'YYYY-MM-DD'. 
                                             Por defecto es '2000-01-01'.
-            fecha_fin (datetime, optional): La fecha de fin de consulta tipo datetime  para obtener datos en formato 'YYYY-MM-DD'.
+            end_time (datetime, optional): La fecha de fin de consulta tipo datetime  para obtener datos en formato 'YYYY-MM-DD'.
                                             Por defecto es la fecha actual.
             variacion (str, optional): Parámetro opcional que define si se desea obtener los incrmentos porcentuales de datos de la serie con respecto a observaciones anteriores
                                     ('PorcObsAnt', 'PorcAnual', 'PorcAcumAnual'). Por defecto es None.
@@ -108,12 +111,18 @@ class Fred(BaseAPI):
             >>> df, dict = get_SIE_data(serie_id='SF43718', ult_obs=True)
 
             Obtener un rango de fechas para una serie histórica de su variación anual:
-            >>> df, dict = get_SIE_data(serie_id='SF43718', fecha_inicio='2020-01-01', fecha_fin='2023-01-01', variacion='PorcAnual')
+            >>> df, dict = get_SIE_data(serie_id='SF43718', fecha_inicio='2020-01-01', end_time='2023-01-01', variacion='PorcAnual')
         """
 
         # Definir la URL de la API con el ID de la serie para obtener los datos de las series y realizar la solicitud
-        endpoint_datos = self._set_params(self,serie_id, last_data, start_time, end_time)
-        data_json = self._make_request(endpoint_datos)
+        endpoint = self._set_params(serie_id, last_data, start_time, end_time)
+        data_json = self._make_request(endpoint)
 
+        # Inicializar un DataFrame vacío para almacenar los datos de las series
+        series_df = pd.DataFrame()
+        
+        for serie_data in data_json['observations']:
+            pass
+        
         return data_json
 
